ca80c4647dcfb78722d9e0f5301b5e474ce7c7c6
Kiarie <geofreykiarie04@gmail.com>
2026-06-19 16:50:01 +0300

    Docker container

--- main.py
+++ main.py
@@ -21,6 +21,3 @@ app.include_router(auth.router)
 app.include_router(pipeline.router)
 app.include_router(insights.router)
 
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
